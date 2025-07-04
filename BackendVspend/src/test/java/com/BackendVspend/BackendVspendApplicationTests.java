35825e93f20f7c7115adb99d5893bd513167fb26
VarunCypherV <varunvk2004@gmail.com>
2025-07-04 18:48:30 +0530

    chore: blank handle

--- BackendVspend/src/test/java/com/BackendVspend/BackendVspendApplicationTests.java
+++ BackendVspend/src/test/java/com/BackendVspend/BackendVspendApplicationTests.java
@@ -2,8 +2,10 @@ package com.backendvspend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // ✅ Activate test profile during CI
 class BackendVspendApplicationTests {
 
 	@Test
