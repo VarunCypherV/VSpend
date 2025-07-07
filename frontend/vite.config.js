4cd8a76b7d951908dcbbfa3867b579be121451e0
VarunCypherV <varunvk2004@gmail.com>
2025-07-07 19:03:38 +0530

    feat: Corrections in FE

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'import.meta.env.VITE_SPRING_API_URL': JSON.stringify(process.env.VITE_SPRING_API_URL)
-  },
+  plugins: [react()]
 })
 
